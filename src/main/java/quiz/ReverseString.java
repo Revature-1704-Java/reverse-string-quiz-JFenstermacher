20c1bcaa2bf1f89f7d3214255b314e227da1286d
JFen <JFenstermacher123@gmail.com>
2017-12-20 10:11:58 -0500

    Added null behavior

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,10 +2,12 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
+    	if (input == null) return ""; //If null string, return empty one
+    	
         StringBuilder sb = new StringBuilder();
         
         for (int i = input.length() - 1; i >= 0; i--)
-        	sb.append(input.charAt(i));
+        	sb.append(input.charAt(i)); //Build the string in reverse
         
     	return sb.toString();
     }
